0df6ea1399573fa4993af3e0597e87b3ac813f9d
Irina Gaynanova <irina.gain@gmail.com>
2014-08-28 14:12:42 -0400

    Version 1.1

--- MGSDA/R/dLDA.R
+++ MGSDA/R/dLDA.R
@@ -1,4 +1,4 @@
-dLDA <-function(xtrain,ytrain,lambda,Vinit=NULL){ 
+dLDA <-function(xtrain,ytrain,lambda,Vinit=NULL,eps=1e-6){ 
   if (nrow(xtrain)!=length(ytrain)){
     stop("Dimensions of xtrain and ytrain don't match!")
   } 
@@ -8,7 +8,7 @@ dLDA <-function(xtrain,ytrain,lambda,Vinit=NULL){
   }
   
   fsd=apply(xtrain,2,sd)
-  if (any(fsd)<1e-13){
+  if (any(fsd<1e-13)){
       stop(paste("Some features have standard deviation less than 1e-13!",sep=""))
   }
   #center and scale X
@@ -31,12 +31,12 @@ dLDA <-function(xtrain,ytrain,lambda,Vinit=NULL){
     D=.constructD(Xadj,ytrain)
   
     if (is.null(Vinit)) {
-      V=.solveVcoordf2(W=crossprod(Xadj)/(length(ytrain)-1),D=D,lambda=lambda)
+      V=.solveVcoordf2(W=crossprod(Xadj)/(length(ytrain)-1),D=D,lambda=lambda,eps=eps)
     } else {
       if ((nrow(Vinit)!=ncol(xtrain))|(ncol(Vinit)!=G-1)){
         stop("Supplied initial value for Vinit has wrong dimensions!")
       }
-      V=.solveVcoordf2(W=crossprod(Xadj)/(length(ytrain)-1),D=D,lambda=lambda,V=Vinit)
+      V=.solveVcoordf2(W=crossprod(Xadj)/(length(ytrain)-1),D=D,lambda=lambda,V=Vinit,eps=eps)
     }
   }
   
