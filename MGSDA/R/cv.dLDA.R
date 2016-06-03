2ca5f9dd5046df69fcfc4143691e03a76d4de2a9
Irina <irinag@stat.tamu.edu>
2016-06-03 11:30:56 -0500

    Streamlined the code for G=2 and G>2 cases

--- MGSDA/R/cv.dLDA.R
+++ MGSDA/R/cv.dLDA.R
@@ -1,8 +1,8 @@
 cv.dLDA<-function(Xtrain,Ytrain,lambdaval=NULL,nl=100,msep=5,eps=1e-6,l_min_ratio=ifelse(n<p,0.1,0.0001),myseed=NULL,prior=TRUE,rho=1){
     
-  if (any(is.na(Xtrain))|any(is.na(Ytrain))) 
+  if (any(is.na(Xtrain))|any(is.na(Ytrain))){ 
     stop("Missing values are not allowed!")
-  
+  }      
   n <- length(Ytrain)
   if (nrow(Xtrain)!=n){
     stop(paste("Number of observations in Ytrain (",n,") doesn't match the number of rows in Xtrain, (",nrow(Xtrain),")",sep=""))
@@ -14,120 +14,52 @@ cv.dLDA<-function(Xtrain,Ytrain,lambdaval=NULL,nl=100,msep=5,eps=1e-6,l_min_rati
   G <- max(Ytrain)
   p <- ncol(Xtrain)
   
+  # Set seed for the assignment to folds
   if (!is.null(myseed)){
       set.seed(myseed)
   }
+  
+  # Divide samples into folds
   id <- 1:n
   for (i in 1:G){
-      id[Ytrain==i] <- sample(rep(seq_len(msep),length.out=sum(Ytrain==i)))
+      id[Ytrain == i] <- sample(rep(seq_len(msep),length.out = sum(Ytrain == i)))
   }
   
-  if (G==2){
-      Z <- matrix(0,n,2)
-      for (g in 1:2){
-          Z[Ytrain==g,g]=1
-      }
-      n1 <- sum(Ytrain==1)
-      n2 <- n-n1
-      Ytilde <- sqrt(n1*n2)*Z%*%c(1/n1,-1/n2)
-      if (rho!=1){
-          X_w <- scale(Xtrain)+(sqrt(rho)-1)*(Ytilde%*%crossprod(Ytilde,scale(Xtrain)))/n
-          Y_w <- Ytilde/sqrt(rho)
-      }else{
-          X_w <- scale(Xtrain)
-          Y_w <- Ytilde
-      }
-
-    #calculate lambda path
-    l_max <- max(abs(crossprod(scale(Xtrain),Ytilde)))/n
-    if (!is.null(lambdaval)){
-        lambdaval <- lambdaval[lambdaval<=l_max]
-        nl <- length(lambdaval)
-        if (nl<2) stop("There should be at least two lambdas")
-    }
-    else {
-        lambdaval <- 10^seq(log10(l_min_ratio*l_max),log10(l_max),length.out=nl)
-    }
-    lambdaval <- sort(lambdaval,decreasing=T)
+  # Form matrix Ytilde and make adjustment if rho!=1
+  Ytilde <- .createY(Ytrain)
+  if (rho != 1){
+      X_w <- scale(Xtrain)+(sqrt(rho)-1)*(Ytilde%*%crossprod(Ytilde,scale(Xtrain)))/n
+      Y_w <- Ytilde/sqrt(rho)
+  }else{
+      X_w <- scale(Xtrain)
+      Y_w <- Ytilde
+  }
   
-    error <- matrix(1, n, nl)
-    features <- matrix(min(n,p),msep,nl)
-      
-    cat("Fold")
-    for (i in 1:msep){   
-      cat(i)
-      xtrain <- Xtrain[id!=i,]
-      Xadj <- scale(xtrain)
-      coef <- attr(Xadj,which="scaled:scale")
-      mtrain <- attr(Xadj,which="scaled:center")
-      ytrain <- Ytrain[id!=i]
-      xtest <- Xtrain[id==i,]
-      xtest <- scale(xtest,center=mtrain,scale=coef) 
-      ytest <- Ytrain[id==i]
-      
-      nnew <- length(ytrain)
-      Znew <- Z[id!=i,]
-      n1 <- sum(ytrain==1)
-      n2 <- nnew-n1
-      Ytilde <- sqrt(n1*n2)*Znew%*%c(1/n1,-1/n2)
-      if (rho!=1){
-          X_w <- Xadj+(sqrt(rho)-1)*(Ytilde%*%crossprod(Ytilde,Xadj))/nnew
-          Y_w <- Ytilde/sqrt(rho)
-      }else{
-          X_w <- Xadj
-          Y_w <- Ytilde
-      }
-      
-      V <- matrix(0,p,1)
-
-      for (j in 1:nl){  
-        V <- .solveMyLasso_c(X_w,Y_w,lambda=lambdaval[j],binit=V)
-        ypred <- classifyV(Xadj,ytrain,xtest,V,prior=prior)
-        error[id == i, j] <- ypred != ytest 
-        features[i,j] <- sum(V!=0)
-        if (features[i,j]>=min(n,p)){
-            break
-        }
-      }
-    }
-    error_mean <- colMeans(error)
-    error_se <- apply(error,2,sd)/sqrt(n)
-    j <- which.min(error_mean)
-    obj <- list(lambda_min=lambdaval[j],error_mean=error_mean,error_se=error_se, f=round(colMeans(features)),lambdaval=lambdaval)
-    return(obj) 
-  } else{
-    #multiple group case
-    Ytilde <- .createY(Ytrain)
-    if (rho!=1){
-        X_w <- scale(Xtrain)+(sqrt(rho)-1)*(Ytilde%*%crossprod(Ytilde,scale(Xtrain)))/n
-        Y_w <- Ytilde/sqrt(rho)
-    }else{
-        X_w <- scale(Xtrain)
-        Y_w <- Ytilde
-    }
-    l_max <- max(abs(crossprod(scale(Xtrain),Ytilde)))/n
-    if (!is.null(lambdaval)){
-        lambdaval <- lambdaval[lambdaval<=l_max]
-        nl <- length(lambdaval)
-        if (nl<2) stop("There should be at least two lambdas")
-    }else{
-        lambdaval <- 10^seq(log10(l_min_ratio*l_max),log10(l_max),length.out=nl)
-    }
-    lambdaval <- sort(lambdaval,decreasing=T)
-    
-    error <- matrix(1,n,nl) 
-    features <- matrix(min(n,p),msep,nl)
+  # Calculate lambda path
+  l_max <- max(abs(crossprod(scale(Xtrain),Ytilde)))/n
+  if (!is.null(lambdaval)){
+      lambdaval <- lambdaval[lambdaval <= l_max]
+      nl <- length(lambdaval)
+      if (nl < 2) stop("There should be at least two lambdas")
+  }else{
+      lambdaval <- 10^seq(log10(l_min_ratio*l_max),log10(l_max),length.out = nl)
+  }
+  lambdaval <- sort(lambdaval,decreasing=T)
+  
+  # Store errors and the number of selected features
+  error <- matrix(1, n, nl)
+  features <- matrix(min(n,p),msep,nl)
   
     cat("Fold")
     for (i in 1:msep){   
       cat(i)
       xtrain <- Xtrain[id!=i,]
       Xadj <- scale(xtrain)
-      coef <- attr(Xadj,which="scaled:scale")
-      mtrain <- attr(Xadj,which="scaled:center")
+      coef <- attr(Xadj, which = "scaled:scale")
+      mtrain <- attr(Xadj, which = "scaled:center")
       ytrain <- Ytrain[id!=i]
       xtest <- Xtrain[id==i,]
-      xtest <- scale(xtest,center=mtrain,scale=coef) 
+      xtest <- scale(xtest, center = mtrain, scale = coef) 
       ytest <- Ytrain[id==i]
       
       Ytilde <- .createY(ytrain)
@@ -140,24 +72,27 @@ cv.dLDA<-function(Xtrain,Ytrain,lambdaval=NULL,nl=100,msep=5,eps=1e-6,l_min_rati
       }
       V <- matrix(0,p,G-1)
     
-      for (j in 1:nl){  
-        V <- .solveMyLassoF_c(X_w,Y_w,lambda=lambdaval[j],binit=V)
-        features[i,j] <- sum(rowSums(V)!=0) 
-      
+      for (j in 1:nl){
+        if (G != 2){
+            V <- .solveMyLassoF_c(X_w,Y_w,lambda=lambdaval[j],binit=V)
+            features[i,j] <- sum(rowSums(V) != 0) 
+        }else{
+            V <- .solveMyLasso_c(X_w,Y_w,lambda=lambdaval[j],binit=V)
+            features[i,j] <- sum(V != 0)
+        }
         if (features[i,j]>min(n,p-1)){
           ytestpred <- classifyV(Xadj,ytrain,xtest,V,prior=prior)
-          error[id==i,j] <- ytestpred!=ytest
+          error[id == i, j] <- ytestpred != ytest
           break
         }else if (features[i,j]>0){
           ytestpred <- classifyV(Xadj,ytrain,xtest,V,prior=prior)
-          error[id==i,j] <- ytestpred!=ytest
+          error[id == i, j] <- ytestpred != ytest
        }
-    }
-  }
+    } # end loop for lambda
+  }# end loop for folds
     error_mean <- colMeans(error)
     error_se <- apply(error,2,sd)/sqrt(n)
-  j <- which.min(error_mean)
-  obj <- list(lambda_min=lambdaval[j],error_mean=error_mean, error_se = error_se, f=round(colMeans(features)),lambdaval=lambdaval)
-  return(obj) 
-  }
+    j <- which.min(error_mean)
+    obj <- list(lambda_min = lambdaval[j],error_mean = error_mean, error_se = error_se, f = round(colMeans(features)),lambdaval = lambdaval)
+    return(obj) 
 }
--- MGSDA/R/dLDA.R
+++ MGSDA/R/dLDA.R
@@ -1,50 +1,38 @@
-dLDA <-function(xtrain,ytrain,lambda,Vinit=NULL,eps=1e-6,maxiter=1000,rho=1){ 
-  if (nrow(xtrain)!=length(ytrain)){
-    stop("Dimensions of xtrain and ytrain don't match!")
-  } 
-  
-  if (any(is.na(xtrain))|any(is.na(ytrain))){
-    stop("Missing values are not allowed")
-  }
-  
-  if (any(apply(xtrain,2,sd) < 1e-13)){
-      stop(paste("Some features have standard deviation less than 1e-13!",sep=""))
-  }
-  G <- max(ytrain)
-  if (!is.null(Vinit)) {
-      if ((nrow(Vinit)!=ncol(xtrain))|(ncol(Vinit)!=G-1)){
-          stop("Supplied initial value for Vinit has wrong dimensions!")
+dLDA <- function(xtrain, ytrain, lambda, Vinit=NULL, eps=1e-6, maxiter=1000, rho=1){ 
+    if (any(is.na(xtrain))|any(is.na(ytrain))){
+      stop("Missing values are not allowed")
     }
-  }
-  
-  #center and scale X
-  Xadj <- scale(xtrain)
-  coef <- attr(Xadj,which="scaled:scale")
-   
-  if (G==2){
-    n <- length(ytrain)
-    Z <- matrix(0,n,2)
-    for (g in 1:2){
-        Z[ytrain==g,g] <- 1
-    }
-    n1 <- sum(ytrain==1)
-    n2 <- n-n1
-    Ytilde <- sqrt(n1*n2)*Z%*%c(1/n1,-1/n2)
-    if (rho!=1){
-        Xadj <- Xadj + (sqrt(rho)-1)*(Ytilde%*%crossprod(Ytilde,Xadj))/n
-        Ytilde <- Ytilde/sqrt(rho)
-    }
-    V <- .solveMyLasso_c(Xadj,Ytilde,lambda=lambda,eps=eps,maxiter=maxiter,binit=Vinit)
     
-  } else {   
+    n <- length(ytrain)
+    if (nrow(xtrain) != n){
+        stop("Dimensions of xtrain and ytrain don't match!")
+    } 
+  
+    if (any(apply(xtrain,2,sd) < 1e-13)){
+      stop(paste("Some features have standard deviation less than 1e-13!", sep = ""))
+    }
+    G <- max(ytrain)
+    if (!is.null(Vinit)){
+      if ((nrow(Vinit) != ncol(xtrain))|(ncol(Vinit) != G-1)){
+          stop("Supplied initial value for Vinit has wrong dimensions!")
+        }
+    }
+  
+    #center and scale X
+    Xadj <- scale(xtrain)
+    coef <- attr(Xadj, which = "scaled:scale")
+    
     Ytilde <- .createY(ytrain)  
-    if (rho!=1){
-        Xadj <- Xadj+(sqrt(rho)-1)*(Ytilde%*%crossprod(Ytilde,Xadj))/nrow(xtrain)
+    if (rho != 1){
+        Xadj <- Xadj + (sqrt(rho) - 1)*(Ytilde %*% crossprod(Ytilde,Xadj))/nrow(xtrain)
         Ytilde <- Ytilde/sqrt(rho)
     }
-    V <- .solveMyLassoF_c(Xadj,Ytilde,lambda=lambda,eps=eps,maxiter=maxiter,binit=Vinit) #works
-  }
+    if (G == 2){
+        V <- .solveMyLasso_c(Xadj, Ytilde, lambda=lambda, eps=eps, maxiter=maxiter, binit=Vinit)
+    }else{
+        V <- .solveMyLassoF_c(Xadj, Ytilde, lambda=lambda, eps=eps, maxiter=maxiter, binit=Vinit) 
+    }
   
-  V[abs(V)<eps] <- 0
-  diag(1/coef)%*%V
+  V[abs(V) < eps] <- 0
+  diag(1/coef) %*% V
 }
